b1cd9e3c4630eeba2b5e674ef5fbb238d658f849
helloitsjoe <jboyle617@gmail.com>
2019-08-02 00:05:06 -0400

    react webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const {makeWebpackConfig} = require('webpack-simple');
 
-const config = makeWebpackConfig();
+// const config = makeWebpackConfig();
 
-// const config1 = makeWebpackConfig({
-//   entry: './src/index.js',
-//   output: {
-//     path: `${__dirname}/dist/`,
-//     filename: `main.js`,
-//     library: 'react-hooks-compose',
-//     libraryTarget: 'umd',
-//   },
-//   devtool: 'source-map',
-// });
+const config = makeWebpackConfig({
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist/`,
+    filename: `main.js`,
+    library: 'react-hooks-compose',
+    libraryTarget: 'umd',
+  },
+  devtool: 'source-map',
+});
 
 config.externals = {
   react: 'react'
